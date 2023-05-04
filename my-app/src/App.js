3b1cd69ebd8f09b7ce84aff8d64005c858fb80e8
Bryan Alvarez <balvarez@LABA.PSWAS.LOCAL>
2023-05-04 16:24:15 -0700

    finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import logo from './pokeball-transparent-png-2.png';
 import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [Users, setUsers] = useState([])
+  useEffect(()=>{
+    const getUsers = async () => {
+      let promises = []
+      for (let i = 1; i <= 151; i++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const jsonData = await response.json();
+        promises.push(jsonData)
+      }
+      setUsers(promises)
+      
+    };
+    return () => {
+      console.log("Is this first render?");
+      getUsers()
+    };
+  }, [])
+
+  const User = ({name, type, image}) =>{
+    return(
+      <li>
+          <img className="card-image" src={image}/>
+          <h2 className="card-title"> {name}</h2>
+          <p className="card-subtitle">Type: {type}</p>
+      </li>
+    )
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="PokemonDiv">
+      <div className='Header'>
+        <img src={logo} className='App-logo'/>
+        <h1>All First Gen Pokemon</h1>
+        <img src={logo} className='App-logo'/>
+      </div>
+      <div className='pokemonContainer'>
+        {Users.map((pokemon, index) => (
+          <User  key={index} name={pokemon.name} type={pokemon.types.map((type) => type.type.name).join(', ')} image={pokemon.sprites.front_default}/>
+        ))}
+      </div>
     </div>
   );
 }
